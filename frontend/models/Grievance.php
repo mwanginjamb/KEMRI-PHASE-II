a1100297fce8559b8da1ab1577a5be579e6c4b98
mwanginjamb <francnjamb@gmail.com>
2022-03-17 13:27:02 +0300

    Refactor: Update Grievance form and its logic

--- frontend/models/Grievance.php
+++ frontend/models/Grievance.php
@@ -35,8 +35,14 @@ public $HRO_Findings;
 public $Complaint_Classification;
 public $Employee_Comments;
 public $Severity_of_grievance;
-public  $attachment;
+public $attachment;
 public $Witness_Type;
+
+public $HRM_Emp_No;
+public $HRM_Emp_Name;
+public $HRM_User_ID;
+public $HRM_Comment;
+public $HRM_Rejection_Comments;
 
 
 
