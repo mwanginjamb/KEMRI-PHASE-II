cc66e67f5e004eeb4a58e9acf18e479aa8896409
mwanginjamb <francnjamb@gmail.com>
2022-05-06 10:53:20 +0300

    Chore: SYnc dev code to uat code

---
+++ frontend/views/grievance/_buttons.php
@@ -0,0 +1,115 @@
+<?php
+
+use yii\bootstrap4\Html;
+?>
+<?= ($model->Status == 'New' && $model->Employee_No == Yii::$app->user->identity->{'Employee No_'}) ? Html::a('<i class="fas fa-forward"></i>To HRO', ['send-to-hro'], [
+    'class' => 'btn btn-app bg-success btn-success',
+    'data' => [
+        'confirm' => 'Are you sure you want to send this Document to HRO?',
+        'params' => [
+            'No' => $model->No
+        ],
+        'method' => 'post',
+    ],
+    'title' => 'Send Document to HRO for Acceptance'
+
+]) : '' ?>
+
+
+<?= ($model->Status == 'HRO') ? Html::a('<i class="fas fa-forward"></i> To HoH.', ['send-to-hoh'], [
+    'class' => 'btn btn-app bg-success mx-1',
+    'data' => [
+        'confirm' => 'Are you sure you want to send this Document to HOH?',
+        'params' => [
+            'No' => $model->No,
+        ],
+        'method' => 'post',
+    ],
+    'title' => 'To HoH.'
+
+]) : ''
+
+
+?>
+
+
+<?= ($model->Status == 'HRO') ? Html::a('<i class="fas fa-times"></i> Withdraw.', ['withdraw'], [
+    'class' => 'btn btn-app bg-success mx-1',
+    'data' => [
+        'confirm' => 'Are you sure you want to withdraw this grievance ?',
+        'params' => [
+            'No' => $model->No,
+        ],
+        'method' => 'post',
+    ],
+    'title' => 'Withdraw Grievance.'
+
+]) : ''
+
+
+?>
+
+
+<?= ($model->Status == 'HRO') ? Html::a('<i class="fas fa-check"></i> Accept ', ['accept-grievance'], [
+    'class' => 'btn btn-app bg-success mx-1',
+    'data' => [
+        'confirm' => 'Are you sure you want to Accept this Grievance?',
+        'params' => [
+            'No' => $model->No,
+        ],
+        'method' => 'post',
+    ],
+    'title' => 'Accept Grievance.'
+
+]) : ''
+?>
+
+
+<?= ($model->Status == 'HRO') ? Html::a('<i class="fas fa-times"></i> Reject ', ['reject-grievance'], [
+    'class' => 'btn btn-app bg-success mx-1',
+    'data' => [
+        'confirm' => 'Are you sure you want to Accept this Grievance?',
+        'params' => [
+            'No' => $model->No,
+        ],
+        'method' => 'post',
+    ],
+    'title' => 'Reject Grievance.'
+
+]) : ''
+?>
+
+
+    
+
+
+<?= ($model->Status == 'Accepted') ? Html::a('<i class="fas fa-forward"></i> To Disciplinary ', ['convert-to-disciplinary'], [
+    'class' => 'btn btn-app bg-success mx-1',
+    'data' => [
+        'confirm' => 'Are you sure you want to Convert this grievance to a disciplinary case?',
+        'params' => [
+            'No' => $model->No,
+        ],
+        'method' => 'post',
+    ],
+    'title' => 'Covert to Disciplinary Case.'
+
+]) : ''
+?>
+
+
+
+
+<?= ($model->Status == 'HOH') ? Html::a('<i class="fas fa-times"></i> Close ', ['close-grievance'], [
+    'class' => 'btn btn-app bg-success mx-1',
+    'data' => [
+        'confirm' => 'Are you sure you want to close grievance?',
+        'params' => [
+            'No' => $model->No,
+        ],
+        'method' => 'post',
+    ],
+    'title' => 'Close Grievance.'
+
+]) : ''
+?>
