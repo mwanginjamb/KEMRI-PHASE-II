4a85452598596cd1cf18fe540cba04b037ac84bc
mwanginjamb <francnjamb@gmail.com>
2022-06-17 02:54:17 +0300

    Refactor: Provision for grant activities irregardless of the grant

--- frontend/views/Donorline/_form.php
+++ frontend/views/Donorline/_form.php
@@ -1,12 +1,15 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: HP ELITEBOOK 840 G5
  * Date: 2/24/2020
  * Time: 12:13 PM
  */
+
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
+
 $absoluteUrl = \yii\helpers\Url::home(true);
 ?>
 
@@ -18,45 +21,45 @@ $absoluteUrl = \yii\helpers\Url::home(true);
             </div>
             <div class="card-body">
 
-                    <?php
-                    $form = ActiveForm::begin([
-                        'id' => $model->formName()
-                    ]); ?>
+                <?php
+                $form = ActiveForm::begin([
+                    'id' => $model->formName()
+                ]); ?>
                 <div class="row">
 
-                            <div class="col-md-6">
-                                    <?= $form->field($model, 'Key')->hiddenInput(['readonly' => true])->label(false) ?>
-                                    <?= $form->field($model, 'Grant_Code')->dropDownList($donors, ['prompt' => 'Select Contract Code...']) ?>
-                                    
-                                    <?= $form->field($model, 'Contract_Grant_Start_Date')->textInput(['type' => 'date','min' => date('Y-m-d', strtotime($model->Grant_Start_Date)), 'max' => date('Y-m-d', strtotime($model->Grant_End_Date)) ]) ?>
-                                    <?= $form->field($model, 'Contract_Grant_End_Date')->textInput(['type' => 'date','min' => date('Y-m-d', strtotime($model->Grant_Start_Date)),'max' => date('Y-m-d', strtotime($model->Grant_End_Date))  ]) ?>
-                                    <?= $form->field($model, 'Percentage')->textInput(['type' => 'number']) ?>
-                                    <?php $form->field($model, 'Grant_Status')->dropDownList([
-                                        '_blank_' => '_blank_',
-                                        'Active' => 'Active',
-                                        'Expired' => 'Expired',
-                                        'Inactive' => 'Inactive'
-                                    ],['prompt' => 'Select ...','readonly' => true]) ?>
-                                    <?= $form->field($model, 'Employee_No')->hiddenInput(['readonly' => true])->label(false) ?>
-                                    <?= $form->field($model, 'Contract_Code')->hiddenInput(['readonly' => true])->label(false) ?>
-                                    <?= $form->field($model, 'Change_No')->hiddenInput(['readonly' => true])->label(false) ?>
-                                    <?= $form->field($model, 'Contract_Line_No')->hiddenInput(['readonly' => true])->label(false) ?>
-                                    <?= $form->field($model, 'Line_No')->hiddenInput(['readonly' => true, 'disabled' => true])->label(false) ?>
-                                   
-                            </div>
+                    <div class="col-md-6">
+                        <?= $form->field($model, 'Key')->hiddenInput(['readonly' => true])->label(false) ?>
+                        <?= $form->field($model, 'Grant_Code')->dropDownList($donors, ['prompt' => 'Select Contract Code...']) ?>
 
-                             <div class="col-md-6">
-                                <?php $form->field($model, 'Grant_Name')->textInput(['readonly' => true, 'disabled' => true]) ?>
+                        <?= $form->field($model, 'Contract_Grant_Start_Date')->textInput(['type' => 'date', 'min' => date('Y-m-d', strtotime($model->Grant_Start_Date)), 'max' => date('Y-m-d', strtotime($model->Grant_End_Date))]) ?>
+                        <?= $form->field($model, 'Contract_Grant_End_Date')->textInput(['type' => 'date', 'min' => date('Y-m-d', strtotime($model->Grant_Start_Date)), 'max' => date('Y-m-d', strtotime($model->Grant_End_Date))]) ?>
+                        <?= $form->field($model, 'Percentage')->textInput(['type' => 'number']) ?>
+                        <?php $form->field($model, 'Grant_Status')->dropDownList([
+                            '_blank_' => '_blank_',
+                            'Active' => 'Active',
+                            'Expired' => 'Expired',
+                            'Inactive' => 'Inactive'
+                        ], ['prompt' => 'Select ...', 'readonly' => true]) ?>
+                        <?= $form->field($model, 'Employee_No')->hiddenInput(['readonly' => true])->label(false) ?>
+                        <?= $form->field($model, 'Contract_Code')->hiddenInput(['readonly' => true])->label(false) ?>
+                        <?= $form->field($model, 'Change_No')->hiddenInput(['readonly' => true])->label(false) ?>
+                        <?= $form->field($model, 'Contract_Line_No')->hiddenInput(['readonly' => true])->label(false) ?>
+                        <?= $form->field($model, 'Line_No')->hiddenInput(['readonly' => true, 'disabled' => true])->label(false) ?>
 
-                                <?= $form->field($model, 'Grant_Activity')->dropdownList($activities,['prompt' => 'Select Grant Activity ...']) ?>
-                                <?= $form->field($model, 'Grant_Type')->textInput(['readonly' => true,'disabled' => true]) ?>
-                             </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <?php $form->field($model, 'Grant_Name')->textInput(['readonly' => true, 'disabled' => true]) ?>
+
+                        <?= $form->field($model, 'Grant_Activity')->dropdownList($activities, ['prompt' => 'Select Grant Activity ...']) ?>
+                        <?= $form->field($model, 'Grant_Type')->textInput(['readonly' => true, 'disabled' => true]) ?>
+                    </div>
                 </div>
 
                 <div class="row">
 
                     <div class="form-group">
-                        <?php Html::submitButton(($model->isNewRecord)?'Save':'Update', ['class' => 'btn btn-success','id'=>'submit']) ?>
+                        <?php Html::submitButton(($model->isNewRecord) ? 'Save' : 'Update', ['class' => 'btn btn-success', 'id' => 'submit']) ?>
                     </div>
 
 
@@ -70,7 +73,7 @@ $absoluteUrl = \yii\helpers\Url::home(true);
 <?php
 $script = <<<JS
 
-$('.field-donorline-grant_activity').hide();
+//$('.field-donorline-grant_activity').hide();
 
  $('#donorline-grant_code, #donorline-grant_activity').select2();
 
@@ -80,7 +83,7 @@ $('.field-donorline-grant_activity').hide();
       
 }); 
 
-$('.field-donorline-contract_grant_start_date, .field-donorline-contract_grant_end_date').hover((e) => {
+/*$('.field-donorline-contract_grant_start_date, .field-donorline-contract_grant_end_date').hover((e) => {
     let grantType = $('#donorline-grant_type').val();
         console.log('Grant Type:'+grantType);
        if(grantType)
@@ -92,7 +95,7 @@ $('.field-donorline-contract_grant_start_date, .field-donorline-contract_grant_e
                 $('.field-donorline-grant_activity').fadeOut();
             }
        }
-});
+});*/
 
  $('#donorline-contract_grant_start_date').change((e) => {
         globalFieldUpdate('donorline',false,'Contract_Grant_Start_Date', e);
